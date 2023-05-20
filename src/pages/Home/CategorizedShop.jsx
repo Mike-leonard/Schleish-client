464ba9fcc98f67fe413c4405a92ea92a128024b9
leonard <mortuza.7@gmail.com>
2023-05-20 16:55:49 +0600

    showing data at call tab selected

--- src/pages/Home/CategorizedShop.jsx
+++ src/pages/Home/CategorizedShop.jsx
@@ -1,25 +1,103 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 
+
+const selectedTabClassName = 'text-primary ';
+const selectedTabPanelClassName = 'custom-selected-tab-panel';
+
 const CategorizedShop = () => {
+    const [tabIndex, setTabIndex] = useState(0);
+    const [categoryToy, setCategoryToy] = useState([]);
+    console.log(tabIndex)
+
+    useEffect(() => {
+        let categoryText = ''
+        if(tabIndex === 0)
+            categoryText = 'horse'
+        else if(tabIndex === 1)
+            categoryText = 'dinosaur'
+        else
+            categoryText = 'cat'
+
+        const url = `http://localhost:3000/toys?category=${categoryText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }, [tabIndex])
+
     return (
-        <div>
-            <Tabs>
-                <TabList>
-                    <Tab>Title 1</Tab>
-                    <Tab>Title 2</Tab>
-                    <Tab>Title 3</Tab>
+        <div className="m-5">
+            <Tabs onSelect={(index) => setTabIndex(index)}>
+                <TabList className="text-center bg-slate-200 p-0 text-xl" >
+                    <Tab>Horse</Tab>
+                    <Tab>Dinosaur</Tab>
+                    <Tab>Cat</Tab>
                 </TabList>
 
                 <TabPanel>
-                    <h2>Any content 1</h2>
+                    <div className="grid sm:grid-cols-3 gap-3 w-full mt-1">
+                        <div className="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className="grid sm:grid-cols-3 gap-3 w-full mt-1">
+                        <div className="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </TabPanel>
                 <TabPanel>
-                    <h2>Any content 3</h2>
+                    <div className="grid sm:grid-cols-3 gap-3 w-full mt-1">
+                        <div className="card card-compact bg-base-100 shadow-xl">
+                            <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+                            <div className="card-body">
+                                <h2 className="card-title">Shoes!</h2>
+                                <p>If a dog chews shoes whose shoes does he choose?</p>
+                                <div className="card-actions justify-end">
+                                    <button className="btn btn-primary">Buy Now</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </TabPanel>
             </Tabs>
         </div>
